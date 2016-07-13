b2b98d024a6b29d709000c785a97895df0168d5a
Luis Canales <lacanales@ucdavis.edu>
2016-07-13 12:28:21 -0700

    Adding full table, commenting out bottom portions. Added custom styles Sorted displays.

--- server.js
+++ server.js
@@ -20,21 +20,40 @@ app.get('/', function (req, res) {
     //Table headers:
     //project-name,last deployed dev,last deployed qa,last deployed uat, last deployed prep, last deployed prod
     for (var index in deploymentProjects) {
-      //console.log(deploymentProjects[index]['deploymentProject']['name']);
-      var data = [{
-        'name' : deploymentProjects[index]['deploymentProject']['name'],
-        /*for(var environment in deploymentProjects[index]['environmentStatuses']){
+      for (var env in deploymentProjects[index]['environmentStatuses']){
+        //Check for never deployed environments.
+        if (deploymentProjects[index]['environmentStatuses'][env].hasOwnProperty('deploymentResult')){
+          var unixDate = deploymentProjects[index]['environmentStatuses'][env]['deploymentResult']['finishedDate'];
+          var deployState = deploymentProjects[index]['environmentStatuses'][env]['deploymentResult']['deploymentState'];
+          var deploySummary = deploymentProjects[index]['environmentStatuses'][env]['deploymentResult']['reasonSummary'];
+          var deployVersion = deploymentProjects[index]['environmentStatuses'][env]['deploymentResult']['deploymentVersion']['name'];
 
-        }*/
-        'env'          : deploymentProjects[index]['environmentStatuses'][0]['environment']['name'],
-        'date'      : deploymentProjects[index]['environmentStatuses'][0]['deploymentResult']['finishedDate'],
-        'result'   : deploymentProjects[index]['environmentStatuses'][0]['deploymentResult']['deploymentState']
-      }]
+          deployDate = new Date(unixDate);
+        }
+        else{
+          var deployDate = "Never deployed."
+          var deployState = "N/A"
+          var deploySummary = "N/A"
+          var deployVersion = "N/A"
+        }
 
-      drawData = drawData.concat(data)
+        //Construct data for table
+        var data = [{
+          'name'      : deploymentProjects[index]['deploymentProject']['name'],
+          'env'       : deploymentProjects[index]['environmentStatuses'][env]['environment']['name'],
+          'date'      : deployDate,
+          'result'    : deployState,
+          'version'   : deployVersion,
+          'summary'   : deploySummary
+        }]
+      }
+
+      drawData = drawData.concat(data);
     }
 
-    res.render('index', { title: 'TossDash - Bamboo Deploy Dashboard', data: drawData});
+    var sortedDeploys = sortDeployData(drawData,10);
+
+    res.render('index', { title: 'Glasseye - Bamboo Deploy Dashboard', data: sortedDeploys});
   })
 
 });
@@ -43,3 +62,32 @@ app.get('/', function (req, res) {
 app.listen(8080, function () {
   console.log('TossDash listening on port 8080!');
 });
+
+function sortDeployData(data,limit){
+  var newData = [];
+  data.sort(function(a,b){
+    return b.date - a.date;
+  })
+
+  //Bundle the formatting of the date string since we're running through the whole
+  //array anyway
+  for (var index=0; index <= limit; index++ ){
+    data[index]['date'] = timeConverter(data[index]['date']);
+    newData = newData.concat(data[index]);
+  }
+  return newData;
+};
+
+
+function timeConverter(UnformattedDate){
+  var a = UnformattedDate;
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = UnformattedDate.getFullYear();
+  var month = months[UnformattedDate.getMonth()];
+  var date = UnformattedDate.getDate();
+  var hour = UnformattedDate.getHours();
+  var min = "0" + UnformattedDate.getMinutes();
+  var sec = "0" + UnformattedDate.getSeconds();
+  var time = date + '-' + month + '-' + year + ' ' + ' ' + hour + ':' + min.substr(-2) + ':' + sec.substr(-2) ;
+  return time;
+}
