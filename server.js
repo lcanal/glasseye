07867d0f88f435d79cdceea1dab29587c425897d
Luis Canales <cluchomucho@gmail.com>
2016-07-13 08:31:42 -0700

    Adding json parsing for 1 environment, printing data. No nice table drawing yet.

--- server.js
+++ server.js
@@ -15,7 +15,26 @@ app.get('/', function (req, res) {
     }
   }
   request(options,function(error,response,body){
-    res.render('index', { title: 'TossDash - Bamboo Deploy Dashboard', data: body});
+    var deploymentProjects = JSON.parse(body);
+    var drawData = [];
+    //Table headers:
+    //project-name,last deployed dev,last deployed qa,last deployed uat, last deployed prep, last deployed prod
+    for (var index in deploymentProjects) {
+      //console.log(deploymentProjects[index]['deploymentProject']['name']);
+      var data = [{
+        'name' : deploymentProjects[index]['deploymentProject']['name'],
+        /*for(var environment in deploymentProjects[index]['environmentStatuses']){
+
+        }*/
+        'env'          : deploymentProjects[index]['environmentStatuses'][0]['environment']['name'],
+        'date'      : deploymentProjects[index]['environmentStatuses'][0]['deploymentResult']['finishedDate'],
+        'result'   : deploymentProjects[index]['environmentStatuses'][0]['deploymentResult']['deploymentState']
+      }]
+
+      drawData = drawData.concat(data)
+    }
+
+    res.render('index', { title: 'TossDash - Bamboo Deploy Dashboard', data: drawData});
   })
 
 });
