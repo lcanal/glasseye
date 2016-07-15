4963544834c7fdfa7c46755c7711770824adc9ff
Luis Canales <cluchomucho@gmail.com>
2016-07-15 08:53:05 -0700

    Added error handling for malformed json.

--- public/core.js
+++ public/core.js
@@ -5,31 +5,16 @@ var globalData = [];
 // when landing on the page, get all todos and show them
 $http.get('/api/dashboard')
     .success(function(data) {
+      if (data.length <= 0){
+        $scope.deploys = [{
+          "date" : "Server returned no results."
+        }]
+      }
+      else {
         $scope.deploys = data;
-        console.log(data);
+      }
     })
     .error(function(data) {
         console.log('Error: ' + data);
     });
-
-/* $scope.addRow = function(){
-	$scope.deploys.push(({ 'name':$scope.deploys.name, 'employees': $scope.employees, 'headoffice':$scope.headoffice }));
-	$scope.deploys[0].name='Ey';
-	$scope.employees='';
-	$scope.headoffice='';
-};
-$scope.removeRow = function(name){
-		var index = -1;
-		var comArr = eval( $scope.companies );
-		for( var i = 0; i < comArr.length; i++ ) {
-			if( comArr[i].name === name ) {
-				index = i;
-				break;
-			}
-		}
-		if( index === -1 ) {
-			alert( "Something gone wrong" );
-		}
-		$scope.companies.splice( index, 1 );
-};*/
 }
