92966bfa523edc706d73393d634ad332dac5ac87
Luis A Canales <lacanales@ucdavis.edu>
2016-07-15 14:48:41 -0700

    Adding UI goodness, moving search bar to top banner. Updating angular to major revision.

--- public/core.js
+++ public/core.js
@@ -1,20 +1,20 @@
 var glasseye = angular.module('glasseye', []);
 
-function mainController($scope, $http) {
-var globalData = [];
-// when landing on the page, get all todos and show them
-$http.get('/api/dashboard')
-    .success(function(data) {
-      if (data.length <= 0){
-        $scope.deploys = [{
-          "date" : "Server returned no results."
-        }]
-      }
-      else {
-        $scope.deploys = data;
-      }
-    })
-    .error(function(data) {
-        console.log('Error: ' + data);
-    });
-}
+glasseye.controller("mainController",["$scope", "$http", function ($scope, $http){
+    var globalData = [];
+    // when landing on the page, get all todos and show them
+    $http.get('/api/dashboard')
+          .success(function(data) {
+            if (data.length <= 0){
+              $scope.deploys = [{
+                "date" : "Server returned no results."
+              }]
+            }
+            else {
+              $scope.deploys = data;
+            }
+          })
+          .error(function(data) {
+              console.log('Error: ' + data);
+          });
+}]);
