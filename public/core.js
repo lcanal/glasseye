2a3f024777947603e2414bf7febce493c1b8598d
Luis Canales <cluchomucho@gmail.com>
2016-07-19 08:45:03 -0700

    Added auto refresh. Need to clean up css.

--- public/core.js
+++ public/core.js
@@ -1,20 +1,46 @@
 var glasseye = angular.module('glasseye', ['ngSanitize']);
+var refreshPeriodMins = .5;
 
-glasseye.controller("mainController",["$scope", "$http", function ($scope, $http){
-    var globalData = [];
-    // when landing on the page, get all todos and show them
-    $http.get('/api/dashboard')
-          .success(function(data) {
-            if (data.length <= 0){
-              $scope.deploys = [{
-                "date" : "Server returned no results."
-              }]
-            }
-            else {
-              $scope.deploys = data;
-            }
-          })
-          .error(function(data) {
-              console.log('Error: ' + data);
-          });
+glasseye.controller("mainController",["$scope", "$http", "$interval", function ($scope, $http, $interval){
+    var dataGrabber = function(){
+      var lastUpdated = timeConverter(new Date());
+
+      // when landing on the page, get all todos and show them
+      $http.get('/api/dashboard')
+            .success(function(data) {
+              if (data.length <= 0){
+                $scope.deploys = [{
+                  "date" : "Server returned no results."
+                }]
+              }
+              else {
+                $scope.deploys = data;
+              }
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+
+     $scope.displayMsg = "Last updated: " + lastUpdated;
+   };
+
+   //Display results initially
+   dataGrabber();
+
+   $interval(dataGrabber, 1000 * 60 * refreshPeriodMins);
+
 }]);
+
+
+function timeConverter(UnformattedDate){
+  var a = UnformattedDate;
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = UnformattedDate.getFullYear();
+  var month = months[UnformattedDate.getMonth()];
+  var date = UnformattedDate.getDate();
+  var hour = UnformattedDate.getHours();
+  var min = "0" + UnformattedDate.getMinutes();
+  var sec = "0" + UnformattedDate.getSeconds();
+  var time = date + '-' + month + '-' + year + ' ' + ' ' + hour + ':' + min.substr(-2) + ':' + sec.substr(-2) ;
+  return time;
+}
